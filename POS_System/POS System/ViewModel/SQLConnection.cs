de0f3e33731eee11f446f530300a5f8cc44ced67
MinhajNa <mjdevil1998@gmail.com>
2024-12-23 11:31:37 +0530

    improve the front-end codes

---
+++ POS_System/POS System/ViewModel/SQLConnection.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Configuration;
+
+namespace Sales_Management_System.ViewModel
+{
+    public class SQLConnection
+    {
+        static SqlConnection Connection;
+        //static string ConnectionString = ConfigurationManager.ConnectionStrings["connectionString"].ToString();
+
+        // static string ConnectionString = ConfigurationManager.ConnectionStrings["connectionString"].ToString();
+         static string ConnectionString = @"Data Source=DESKTOP-R4K96JG\SQLEXPRESS;Initial Catalog=SalesManagementWPF; TrustServerCertificate=False; Integrated Security = True";
+
+
+        public static bool DBConnection()
+        {
+            try
+            {
+                Connection = new SqlConnection(ConnectionString);
+                Connection.Open();
+                //MessageBox.Show("Database connected successfully...");
+                Connection.Close();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Database connection failed...", "Sql Connection", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+
+        public static void SqlConnection()
+        {
+            try
+            {
+                if (Connection.State == System.Data.ConnectionState.Closed)
+                {
+                    Connection = new SqlConnection(ConnectionString);
+                    Connection.Open();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Can't Connect DataBase", "Sql Connection", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public static void close_Connection()
+        {
+            if (Connection.State == System.Data.ConnectionState.Open)
+            { Connection.Close(); }
+        }
+
+        public static SqlConnection getConnection()
+        {
+            return Connection;
+        }
+
+        public static bool ISData(string table)
+        {
+            DBConnection();
+            int value = 0;
+            Connection.Open();
+            bool Retrunit = false;
+            var SerchQuary = $"select count(*) from {table};";
+            var Command = new SqlCommand(SerchQuary, Connection);
+            var reader = Command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                value = int.Parse(reader.GetValue(0).ToString());
+                if (value != 0 && value != null)
+                {
+                    Retrunit = true;
+                }
+
+            }
+            Connection.Close();
+
+            return Retrunit;
+
+        }
+
+        public static string SpaficDataISINTable(string table, string Name, string IDColumnname)
+        {
+            DBConnection();
+            string datavalue = "";
+            Connection.Open();
+            bool Retrunit = false;
+            var SerchQuary = $"select {table}.{Name} from {table};";
+            var Command = new SqlCommand(SerchQuary, Connection);
+            var reader = Command.ExecuteReader();
+            while (reader.Read())
+            {
+
+                datavalue = reader.GetValue(0).ToString();
+            }
+            Connection.Close();
+
+            return datavalue;
+        }
+
+        public static string SpaficDataISINTable(string table, string Name, string IDColumnname, string IDvalue)
+        {
+            DBConnection();
+            string datavalue = "";
+            Connection.Open();
+            bool Retrunit = false;
+            var SerchQuary = $"select {table}.{Name} from {table} where {IDColumnname} ={IDvalue}";
+            var Command = new SqlCommand(SerchQuary, Connection);
+            var reader = Command.ExecuteReader();
+            while (reader.Read())
+            {
+
+                datavalue = reader.GetValue(0).ToString();
+            }
+            Connection.Close();
+
+            return datavalue;
+
+        }
+        public static bool IsData(string table_name, string Column_name, string Check_value)
+        {
+
+            bool value = false;
+            try
+            {
+
+                SqlConnection();
+                string Query = $"select * from {table_name} where {Column_name} = " + " '" + Check_value + "'  ";
+                var command = new SqlCommand(Query, Connection);
+                var dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    value = true;
+                }
+                dataReader.Close();
+                command.Dispose();
+                close_Connection();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                value = false;
+            }
+            return value;
+
+        }
+
+        public static string CheckTableData(string table, string Name, string IDColumn)
+        {
+            DBConnection();
+            string datavalue = "";
+            Connection.Open();
+            bool ReturnUnit = false;
+            var SearchQuery = $"select {table}.{Name} from {table};";
+            var Command = new SqlCommand(SearchQuery, Connection);
+            var Reader = Command.ExecuteReader();
+            while (Reader.Read())
+            {
+                datavalue = Reader.GetValue(0).ToString();
+            }
+            Connection.Close();
+            return datavalue;
+        }
+
+
+        //Get value......
+        static public string GetValue(string table, string getColumn, string Column, string value)
+        {
+
+            //SqlCommand Command;
+            //SqlDataReader dataReader;
+
+            string Value = "";
+            try
+            {
+                Connection.Open();
+                var query = $"select {table}.{getColumn} from {table} where {Column} = '" + value + "'";
+                var Command = new SqlCommand(query, Connection);
+                var dataReader = Command.ExecuteReader();
+
+
+                while (dataReader.Read())
+                {
+                    value = dataReader.GetValue(0).ToString();
+                }
+
+                dataReader.Close();
+                Command.Dispose();
+                Connection.Close();
+
+                return value;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return value;
+            }
+        }
+
+
+
+       // Same product name check validation....
+        static public bool IsProduct(string getvalue, string table_name, string column_name, string Check_value)
+        {
+            SqlCommand Command;
+            SqlDataReader dataReader;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            bool value = false;
+            try
+            {
+                //ConnectionString();
+                Connection.Open();
+                string query = $"select {getvalue} from {table_name} where {column_name} = " + "'" + Check_value + "'";
+                Command = new SqlCommand(query, Connection);
+                dataReader = Command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    value = true;
+                }
+                dataReader.Close();
+                Command.Dispose();
+
+                Connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                value = false;
+            }
+            return value;
+        }
+
+    }
+}
---
+++ POS_System/POS System/ViewModel/VMUserAdd.cs
@@ -0,0 +1,180 @@
+﻿using Sales_Management_System.ViewModel;
+using SalesSystem_05._11._2022.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SalesSystem_05._11._2022.ViewModel
+{
+    public class VMUserAdd : VMBase
+    {
+        public VMUserAdd(MDUser user = null)
+        {
+            if (user == null)
+            {
+                //QtyEnable = true;
+                IsUser();
+                btnAdd = "Add";
+                btnCancel = "Cancel";
+            }
+            else
+            {
+                //QtyEnable = false;
+                User = new();
+                User.UserId = user.UserId;
+                User.UserName = user.UserName;
+                User.Password = user.Password;
+                User.IsActive = user.IsActive;
+                btnAdd = "Update";
+                btnCancel = "Cancel";
+            }
+        }
+
+
+
+        public ICommand cmdUserSave { get { return new RelayCommand(UserSave); } }
+
+        void UserSave(object param)
+        {
+            try
+            {
+
+            
+            switch (btnAdd)
+            {
+
+                case "Add":
+                    if (Validation())
+                    {
+                        if (!VMValidation.Combovalidation(User.IsActive))
+                        {
+                            MessageBox.Show("Please select user status", " User", MessageBoxButton.OK, MessageBoxImage.Error);
+                            return;
+                        }
+
+
+                        SQLConnection.SqlConnection();
+                        string query = $"insert into tblUser values('" + User.UserId + "' ,'" + User.UserName + "','" + User.Password + "' ,'" + User.IsActive + "' )";
+                        SqlCommand Command = new SqlCommand(query, SQLConnection.getConnection());
+                        SqlDataAdapter adapter = new SqlDataAdapter();
+                        adapter.InsertCommand = new SqlCommand(query, SQLConnection.getConnection());
+                        adapter.InsertCommand.ExecuteNonQuery();
+                        adapter.Dispose();
+                        Command.Dispose();
+                        SQLConnection.close_Connection();
+                        VMUser.UserRefresh();
+                        userExit.Invoke();
+                        MessageBox.Show("User sucessfully added", "User", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    break;
+
+                case "Update":
+
+                    if (UpdateValidation())
+                    {
+                        if (!VMValidation.Combovalidation(User.IsActive))
+                        {
+                            MessageBox.Show("Please select user status", " User", MessageBoxButton.OK, MessageBoxImage.Error);
+                            return;
+                        }
+
+                        SQLConnection.SqlConnection();
+                        string Query = $"Update  tblUser set UserName ='" + User.UserName + "' ,Password ='" + User.Password + "',IsActive='" + User.IsActive + "' where UserId = '" + User.UserId + "'";
+                        SqlCommand command = new SqlCommand(Query, SQLConnection.getConnection());
+                        SqlDataAdapter Adapter = new SqlDataAdapter();
+                        Adapter.UpdateCommand = new SqlCommand(Query, SQLConnection.getConnection());
+                        Adapter.UpdateCommand.ExecuteNonQuery();
+                        Adapter.Dispose();
+                        command.Dispose();
+                        SQLConnection.close_Connection();
+                        VMUser.UserRefresh();
+                        userExit.Invoke();
+                        MessageBox.Show("User sucessfully Updated", "User", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    }
+                    break;
+            }
+            }
+            catch
+            {
+                MessageBox.Show("Invalid inputs", " User", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private MDUser user;
+
+        public MDUser User
+        {
+            get { return user; }
+            set { user = value; OnPropertyChanged(); }
+        }
+        public void IsUser()
+        {
+            User = new();
+            User.UserId = SQLConnection.ISData("tblUser") ? int.Parse(SQLConnection.CheckTableData("tblUser", "UserId", "UserId")) + 1 : 1;
+        }
+
+        private string _btnAdd;
+
+        public string btnAdd
+        {
+            get { return _btnAdd; }
+            set { _btnAdd = value; OnPropertyChanged(); }
+        }
+
+        private string _btnCancel;
+
+        public string btnCancel
+        {
+            get { return _btnCancel; }
+            set { _btnCancel = value; OnPropertyChanged(); }
+        }
+
+
+        public ICommand cmdUserExit { get { return new RelayCommand(UserExit); } }
+
+        public static Action userExit;
+        void UserExit(object param)
+        {
+            if (MessageBox.Show("Do you want to Cancel ?", "Cancel", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                userExit.Invoke();
+            }
+
+
+        }
+
+
+        public bool Validation()
+        {
+            bool Result = false;
+
+            bool product = VMValidation.UserAddValidation(User.UserName, User.Password);
+
+            if (product)
+            {
+                Result = true;
+            }
+            return Result;
+        }
+
+        public bool UpdateValidation()
+        {
+            bool Result = false;
+
+            bool product = VMValidation.UserUpdateValidation(User.UserName, User.Password);
+
+            if (product)
+            {
+                Result = true;
+            }
+            return Result;
+        }
+    }
+}
