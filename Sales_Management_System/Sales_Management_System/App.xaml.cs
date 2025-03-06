50569c993abf825169440fdd859fff55d0549a49
Kethusha <117493669+KethA06@users.noreply.github.com>
2025-03-06 13:24:53 +0530

    Add files via upload

---
+++ Sales_Management_System/Sales_Management_System/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Sales_Management_System
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
